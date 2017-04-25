8463919f5f97ba5c0ea3e477a66bc8f45595bf74
nlepage <nicolas.lepage@zenika.com>
2017-04-25 11:00:28 +0200

    :ok_hand: Review @frinyvonnick

--- src/array/xor.spec.js
+++ src/array/xor.spec.js
@@ -6,16 +6,18 @@ import xor from './xor'
 describe('Xor', () => {
 
   const withOneAndTwo = { nested: { prop: [1, 2] } }
-  const withOneAndThree = { nested: { prop: [1, 3] } }
-  const withTwoAndThree = { nested: { prop: [2, 3] } }
+  const oneAndThree = [1, 3]
+  const withOneAndThree = { nested: { prop: oneAndThree } }
+  const twoAndThree = [2, 3]
+  const withTwoAndThree = { nested: { prop: twoAndThree } }
 
   it('should xor arrays', () => {
-    expect(xor(withOneAndTwo, 'nested.prop', [2, 3])).to.be.deep.equal(withOneAndThree)
-    expect(xor(withOneAndTwo, 'nested.prop', [1, 3])).to.be.deep.equal(withTwoAndThree)
+    expect(xor(withOneAndTwo, 'nested.prop', twoAndThree)).to.be.deep.equal(withOneAndThree)
+    expect(xor(withOneAndTwo, 'nested.prop', oneAndThree)).to.be.deep.equal(withTwoAndThree)
   })
 
   it('should xor deep undefined to array', () => {
-    expect(xor({}, 'nested.prop', [1, 3])).to.be.deep.equal(withOneAndThree)
-    expect(xor(undefined, 'nested.prop', [1, 3])).to.be.deep.equal(withOneAndThree)
+    expect(xor({}, 'nested.prop', oneAndThree)).to.be.deep.equal(withOneAndThree)
+    expect(xor(undefined, 'nested.prop', oneAndThree)).to.be.deep.equal(withOneAndThree)
   })
 })
