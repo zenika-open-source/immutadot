369aa69a9fff1c9fcfd1aace4ced234f1ec46379
nlepage <nicolas.lepage@zenika.com>
2017-04-26 00:32:09 +0200

    :memo: Add jsdoc and start writing documentation

--- src/array/index.js
+++ src/array/index.js
@@ -1,6 +1,10 @@
 import push from './push'
 import xor from './xor'
 
+/**
+ * Array functions.
+ * @namespace array
+ */
 export {
   push,
   xor,
--- src/array/xor.js
+++ src/array/xor.js
@@ -1,4 +1,15 @@
 import update from '../core/update'
 import xor from 'lodash/xor'
 
+/**
+ * Replaces by an array of unique values that is the symmetric difference of the former array and the given arrays.
+ * @function xor
+ * @memberof array
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {...Array} arrays The arrays to inspect.
+ * @return {Array} Returns the new array of filtered values.
+ * @see {@link https://lodash.com/docs#xor|Lodash} for more information
+ * @since 0.1.6
+ */
 export default update(xor)
