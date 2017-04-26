0ba62e3ce8b07f91a7622d82f04a2aa4b156c1a7
nlepage <nicolas.lepage@zenika.com>
2017-04-26 15:34:35 +0200

    :memo: fix documentation errors and add examples

--- src/array/xor.js
+++ src/array/xor.js
@@ -9,7 +9,8 @@ import xor from 'lodash/xor'
  * @param {Array|string} path The path of the property to set.
  * @param {...Array} arrays The arrays to inspect.
  * @return {Array} Returns the new array of filtered values.
- * @see {@link https://lodash.com/docs#xor|Lodash} for more information
+ * @example xor({ nested: { prop: [1, 2] } }, 'nested.prop', [2, 3]) // => { nested: { prop: [1, 3] } }
+ * @see {@link https://lodash.com/docs#xor|Lodash.xor} for more information
  * @since 0.1.6
  */
 export default update(xor)
--- src/math/add.js
+++ src/math/add.js
@@ -9,7 +9,8 @@ import update from '../core/update'
  * @param {Array|string} path The path of the property to set.
  * @param {number} addend The number to add.
  * @return {Object} Returns the updated object.
- * @see {@link https://lodash.com/docs#add|Lodash} for more information
+ * @example add({ nested: { prop: 2 } }, 'nested.prop', 4) // => { nested: { prop: 6 } }
+ * @see {@link https://lodash.com/docs#add|Lodash.add} for more information
  * @since 0.1.7
  */
 export default update(add)
