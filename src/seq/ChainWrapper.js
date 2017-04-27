f118a3cd8e4193bb8253667e2ebc7d215aa94435
nlepage <nicolas.lepage@zenika.com>
2017-04-27 11:11:26 +0200

    :memo: Add documentation

--- src/seq/ChainWrapper.js
+++ src/seq/ChainWrapper.js
@@ -6,32 +6,59 @@ import * as math from '../math'
 import flow from 'lodash/flow'
 import mapValues from 'lodash/mapValues'
 
-const namespaces = [
-  array,
-  core,
-  lang,
-  math,
-]
-
+/**
+ * Wrapper allowing to make sequences of immutadot functions calls on a value.
+ * Instances are created by calling {@link chain}.
+ * The result of such sequences must be unwrapped with {@link ChainWrapper#value}.
+ * @memberof seq
+ * @see {@link chain} for more information.
+ * @since 0.1.8
+ */
 class ChainWrapper {
 
+  /**
+   * This constructor should not be called directly.
+   * Instances are created by calling {@link chain}.
+   * @param {*} value The value to wrap.
+   * @protected
+   * @see {@link chain} for more information.
+   * @since 0.1.8
+   */
   constructor(value) {
     this._value = value
     this._flow = []
   }
 
+  /**
+   * Add a function call to the sequence.
+   * @param {Function} fn The function to call.
+   * @param {...*} args The arguments for the function call.
+   * @returns {ChainWrapper} The wrapper instance.
+   * @private
+   * @since 0.1.8
+   */
   _call(fn, args) {
     this._flow.push(value => fn(value, ...args))
     return this
   }
 
+  /**
+   * Executes the chain sequence to resolve the unwrapped value.
+   * @returns {Object} Returns the resolved unwrapped value.
+   * @since 0.1.8
+   */
   value() {
     return flow(this._flow)(this._value)
   }
 }
 
 // Add namespaces functions to the ChainWrapper prototpye
-namespaces.forEach(namespace => Object.assign(
+[
+  array,
+  core,
+  lang,
+  math,
+].forEach(namespace => Object.assign(
   ChainWrapper.prototype,
   mapValues(
     namespace,
--- src/seq/chain.js
+++ src/seq/chain.js
@@ -1,3 +1,13 @@
 import ChainWrapper from './ChainWrapper'
 
-export default object => new ChainWrapper(object)
+/**
+ * Creates an immutadot wrapper instance that wraps value allowing to make sequences of immutadot functions calls on it.
+ * The result of such sequences must be unwrapped with {@link ChainWrapper#value}
+ * @function chain
+ * @memberof seq
+ * @param {*} value The value to wrap.
+ * @return {ChainWrapper} Returns the new immutadot wrapper instance.
+ * @see {@link ChainWrapper} for more information
+ * @since 0.1.8
+ */
+export default value => new ChainWrapper(value)
