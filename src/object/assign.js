16dd8aae454188f60691d8144b37d50cb755687f
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-06-14 15:15:28 +0200

    :sparkles: Add object.assign fix #11

---
+++ src/object/assign.js
@@ -0,0 +1,16 @@
+import assign from 'lodash/fp/assign'
+import update from '../core/update'
+
+/**
+ * Applies <code>!</code> to the property.
+ * @function assign
+ * @memberof object
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {Array|Object} sources The source objects.
+ * @return {Object} Returns the updated object.
+ * @example assign({ nested: { a: 1, b: 2 } }, 'nested', { b: 3, c: 4 }) // => { nested: { a:1, b: 3, c: 4 } }
+ * @see {@link core.update|update} for more information.
+ * @since 0.1.5
+ */
+export default update(assign)
