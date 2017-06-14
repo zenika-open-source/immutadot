c7c56ac5d29788190a8d4d0b120b6036fa2b4661
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-06-14 15:39:49 +0200

    :ok_hand: Take care of review of @nlepage

--- src/object/assign.js
+++ src/object/assign.js
@@ -5,15 +5,18 @@ import update from '../core/update'
 const rawAssign = assign.convert(lodashFpConvertOptions)
 
 /**
- * Applies <code>!</code> to the property.
+ * Assigns own enumerable string keyed properties of source objects to the
+ * destination object. Source objects are applied from left to right.
+ * Subsequent sources overwrite property assignments of previous sources.
  * @function assign
  * @memberof object
  * @param {Object} object The object to modify.
  * @param {Array|string} path The path of the property to set.
- * @param {Array|Object} sources The source objects.
+ * @param {...Object} [sources] The source objects.
  * @return {Object} Returns the updated object.
  * @example assign({ nested: { a: 1, b: 2 } }, 'nested', { b: 3, c: 4 }) // => { nested: { a:1, b: 3, c: 4 } }
  * @see {@link core.update|update} for more information.
- * @since 0.1.5
+ * @see {@link https://lodash.com/docs#assign|lodash.assign} for more information.
+ * @since 0.1.12
  */
 export default update(rawAssign)
