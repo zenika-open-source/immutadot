f695091497127abf1e3f5cc4776c8aeadb8dc7fe
Valentin Cocaud <v.cocaud@gmail.com>
2017-06-20 14:58:47 +0200

    :bulb: Fix documentation

--- src/object/pickBy.js
+++ src/object/pickBy.js
@@ -5,18 +5,18 @@ import update from '../core/update'
 const rawPickBy = pickBy.convert(lodashFpConvertOptions)
 
 /**
- * Assigns own enumerable string keyed properties of source objects to the
- * destination object. Source objects are applied from left to right.
- * Subsequent sources overwrite property assignments of previous sources.
- * @function assign
+ * Creates an object composed of the object properties predicate returns truthy
+ * for. The predicate is invoked with two arguments: (value, key).
+ * @function pickBy
  * @memberof object
  * @param {Object} object The object to modify.
  * @param {Array|string} path The path of the property to set.
- * @param {...Object} [sources] The source objects.
+ * @param {function} [predicate={@link https://lodash.com/docs#identity|lodash.identity}] The function invoked per iteration.
  * @return {Object} Returns the updated object.
- * @example assign({ nested: { a: 1, b: 2 } }, 'nested', { b: 3, c: 4 }) // => { nested: { a:1, b: 3, c: 4 } }
+ * @example pickBy({ nested: { a: 1, b: 2, c: 3, d: 4 } }, 'nested', v => v < 3) // => { nested: { a: 1, b: 2 } }
  * @see {@link core.update|update} for more information.
- * @see {@link https://lodash.com/docs#assign|lodash.assign} for more information.
+ * @see {@link https://lodash.com/docs#pickBy|lodash.pickBy} for more information.
+ * @see {@link https://lodash.com/docs#identity|lodash.identity} for more information.
  * @since 0.1.12
  */
 export default update(rawPickBy)
