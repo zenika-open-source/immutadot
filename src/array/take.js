45e9f5e65f0b0821dca08c866dfb242ffa6e6f35
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-07-07 10:25:03 +0200

    :sparkles: Add lodash take function

---
+++ src/array/take.js
@@ -0,0 +1,17 @@
+import _take from 'lodash/fp/take'
+import { convertLodashFp } from '../util/convert'
+
+/**
+ * Creates a slice of array with <code>n</code> elements taken from the beginning.
+ * @function
+ * @memberof array
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {number} n Number of elements to take from the beginning of the array.
+ * @return {Object} Returns the updated object.
+ * @example take({ nested: { prop: [1, 2, 3, 4] } }, 'nested.prop', 2) // => { nested: { prop: [1, 2] } }
+ * @see {@link https://lodash.com/docs#take|lodash.take} for more information.
+ * @since 0.2.0
+ */
+const take = convertLodashFp(_take)
+export default take
---
+++ src/array/take.spec.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+import take from './take'
+
+describe('Take', () => {
+
+  it('should take one element', () => {
+    const original = { nested: { prop: [1, 2, 3] } }
+
+    const final = take(original, 'nested.prop', 1)
+
+    expect(final).toEqual({ nested: { prop: [1] } })
+    expect(original).toEqual({ nested: { prop: [1, 2, 3] } })
+  })
+
+  it('should replace deep undefined with array', () => {
+    expect(take(undefined, 'nested.prop', () => true)).toEqual({ nested: { prop: [] } })
+  })
+})
