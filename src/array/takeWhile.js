e6f6418ec8f1acf53031f39f3aac1e5945391dd8
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-07-08 16:06:16 +0200

    :sparkles: Add lodash takeWhile function

---
+++ src/array/takeWhile.js
@@ -0,0 +1,19 @@
+import _takeWhile from 'lodash/fp/takeWhile'
+import { convertLodashFp } from '../util/convert'
+
+/**
+ * Creates a slice of array with elements taken from the beginning.
+ * Elements are taken until predicate returns falsey.
+ * The predicate is invoked with three arguments: (value, index, array).
+ * @function
+ * @memberof array
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {Function} [predicate={@link https://lodash.com/docs#identity|lodash.identity}] The function invoked per iteration.
+ * @return {Object} Returns the updated object.
+ * @example takeWhile({ nested: { prop: [1, 2, 3, 4] } }, 'nested.prop', v => v < 2) // => { nested: { prop: [1] } }
+ * @see {@link https://lodash.com/docs#takeWhile|lodash.takeWhile} for more information.
+ * @since 0.3.0
+ */
+const takeWhile = convertLodashFp(_takeWhile)
+export default takeWhile
