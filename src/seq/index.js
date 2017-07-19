d91ce238be976b501eca80e6c672cab0e5e46e19
nlepage <nicolas.lepage@zenika.com>
2017-07-19 16:33:45 +0200

    :sparkles: Add seq.ChainWrapper.commit

--- src/seq/index.js
+++ src/seq/index.js
@@ -3,6 +3,7 @@ import { chain } from './chain'
 /**
  * Sequence functions.
  * @namespace seq
+ * @borrows seq.ChainWrapper#commit as #commit
  * @borrows seq.ChainWrapper#value as #value
  * @since 0.1.8
  */
