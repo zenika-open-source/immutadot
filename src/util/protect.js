8722f5d13dd6eda7a891e117059c9e26d6580664
nlepage <nicolas.lepage@zenika.com>
2017-08-30 13:46:31 +0200

    :sparkles: Protect POC

--- src/util/protect.js
+++ src/util/protect.js
@@ -1,18 +1,32 @@
 import chain from 'seq/chain'
 import concat from 'lodash/concat'
+import get from 'lodash/get'
+import isEmpty from 'lodash/isEmpty'
 import isObject from 'lodash/isObject'
 
 class ProtectHandler {
 
-  constructor(chainWrapper, path = []) {
-    this.chainWrapper = chainWrapper
+  constructor(chainWrapperRef, path = []) {
+    this.chainWrapperRef = chainWrapperRef
     this.path = path
   }
 
   get(target, property) {
-    const reference = target[property]
+    const peekedTarget = isEmpty(this.path) ? this._peek() : get(this._peek(), this.path)
+    const reference = peekedTarget[property]
     if (!isObject(reference)) return reference
-    return new Proxy(reference, new ProtectHandler(this.chainWrapper), concat(this.path, property))
+    return new Proxy(reference, new ProtectHandler(this.chainWrapperRef, concat(this.path, property)))
+  }
+
+  set(target, property, value) {
+    this.chainWrapperRef.chainWrapper = this.chainWrapperRef.chainWrapper.set(concat(this.path, property), value)
+    return true
+  }
+
+  _peek() {
+    let peeked
+    this.chainWrapperRef.chainWrapper = this.chainWrapperRef.chainWrapper.peek(_peeked => { peeked = _peeked })
+    return peeked
   }
 }
 
@@ -20,8 +34,10 @@ const protect = reference => {
   if (!isObject(reference)) throw TypeError('Cannot protect a non-object value')
   return callback => {
     const chainWrapper = chain(reference)
-    const proxy = new Proxy(reference, new ProtectHandler(chainWrapper))
-    return callback(proxy, chainWrapper)
+    const chainWrapperRef = { chainWrapper }
+    const proxy = new Proxy(reference, new ProtectHandler(chainWrapperRef))
+    callback(proxy)
+    return chainWrapperRef.chainWrapper
   }
 }
 
--- src/util/protect.spec.js
+++ src/util/protect.spec.js
@@ -1,17 +1,24 @@
 /* eslint-env jest */
+import { immutaTest } from 'test.utils'
 import protect from './protect'
 
 describe('Protect', () => {
   it('should return properties value', () => {
-    const object = {
+    immutaTest(input => {
+      const output = protect(input)(protectedInput => {
+        protectedInput.d = '666'
+        protectedInput.b.c = `🍺 ${protectedInput.d} 🍺`
+      }).value()
+      expect(output).toEqual({
+        a: 1,
+        b: { c: '🍺 666 🍺' },
+        d: '666',
+      })
+      return output
+    }, {
       a: 1,
       b: { c: 3 },
-    }
-
-    protect(object)(proxy => {
-      expect(proxy.a).toEqual(1)
-      expect(proxy.b.c).toEqual(3)
-    })
+    }, 'd', 'b.c')
   })
 
   it('should not proxy non object values', () => {
