f86d6e4e3d01439eac8c5ff294b27f566021602b
Nicolas Lepage <nlepage@users.noreply.github.com>
2017-09-01 10:05:44 +0200

    :sparkles: Experimenting protect feature (#66)

---
+++ src/util/protect.spec.js
@@ -0,0 +1,32 @@
+/* eslint-env jest */
+import { immutaTest } from 'test.utils'
+import protect from './protect'
+
+describe('Protect', () => {
+  it('should return properties value', () => {
+    immutaTest(input => {
+      const output = protect(input)(protectedInput => {
+        protectedInput.a++
+        protectedInput.d = '666'
+        protectedInput.b.c = `🍺 ${protectedInput.d} 🍺`
+        delete protectedInput.e
+      })
+      expect(output).toEqual({
+        a: 2,
+        b: { c: '🍺 666 🍺' },
+        d: '666',
+        other: {},
+      })
+      return output
+    }, {
+      a: 1,
+      b: { c: 3 },
+      e: 'aze',
+      other: {},
+    }, 'a', 'd', 'b.c', 'e')
+  })
+
+  it('should not proxy non object values', () => {
+    expect(() => protect(undefined)).toThrow(TypeError)
+  })
+})
