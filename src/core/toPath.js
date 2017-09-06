a31691e69accaaab815a1a6ea32d62cd86a70633
nlepage <nicolas.lepage@zenika.com>
2017-09-07 00:55:21 +0200

    :bulb: Documentation

--- src/core/toPath.js
+++ src/core/toPath.js
@@ -1,13 +1,32 @@
 import {
+  isIndex,
   isSymbol,
   toString,
 } from 'util/lang'
 
-const isIndex = arg => Number.isSafeInteger(arg) && arg >= 0
-
+/**
+ * Converts a value to a valid path key.<br />
+ * Returns <code>arg</code> if arg is a positive integer or a Symbol, <code>toString(arg)</code> otherwise.
+ * @param {*} arg The value to convert
+ * @return {string} A valid path key
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
 const toKey = arg => isIndex(arg) || isSymbol(arg) ? arg : toString(arg)
 
 const delimiters = ['"', '\'']
+
+/**
+ * Tests whether <code>index</code>th char of <code>str</code> is a delimiter.<br />
+ * Delimiters are <code>"</code> and <code>'</code>.
+ * @param {string} str The string
+ * @param {number} index Index of the char to test
+ * @return {{ delimited: boolean, delimiter: string }} A boolean <code>delimited</code>, true if <code>str.charAt(index)</code> is a delimiter and the <code>delimiter</code>.
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
 const isDelimiterChar = (str, index) => {
   const char = str.charAt(index)
   const delimiter = delimiters.find(c => c === char)
@@ -17,20 +36,57 @@ const isDelimiterChar = (str, index) => {
   }
 }
 
-const toSliceIndex = str => str === '' ? undefined : Number.parseInt(str)
-
 const escapedDelimsRegexps = {}
 for (const delimiter of delimiters)
   escapedDelimsRegexps[delimiter] = new RegExp(`\\\\${delimiter}`, 'g')
 
+  /**
+   * Strip slashes preceding occurences of <code>delimiter</code> from <code>str</code><br />
+   * Possible delimiters are <code>"</code> and <code>'</code>.
+   * @param {string} str The string
+   * @param {string} delimiter The delimiter to unescape
+   * @return {string} The unescaped string
+   * @memberof core
+   * @private
+   * @since 0.4.0
+   */
 const unescapeDelimiters = (str, delimiter) => str.replace(escapedDelimsRegexps[delimiter], delimiter)
 
+/**
+ * Converts <code>str</code> to a slice index.
+ * @param {string} str The string to convert
+ * @return {number} <code>undefined</code> if <code>str</code> is empty, otherwise an int (may be NaN)
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
+const toSliceIndex = str => str === '' ? undefined : Number.parseInt(str)
+
+/**
+ * Wraps <code>fn</code> allowing to call it with an array instead of a string.<br />
+ * The returned function behaviour is :<br />
+ *  - If called with an array, returns a copy of the array with values converted to path keys<br />
+ *  - Otherwise, calls <code>fn</code> with the string representation of its argument
+ * @param {function} fn The function to wrap
+ * @return {function} The wrapper function
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
 const allowingArrays = fn => arg => {
   if (Array.isArray(arg)) return arg.map(toKey)
 
   return fn(toString(arg))
 }
 
+/**
+ * Converts <code>str</code> to a path represented as an array of keys.
+ * @param {string} str The string to convert
+ * @return {(string|number)[]} The path represented as an array of keys
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
 const stringToPath = str => {
   const path = []
   let index = 0
@@ -111,6 +167,16 @@ const stringToPath = str => {
 
 const MAX_CACHE_SIZE = 1000
 const cache = new Map()
+
+/**
+ * Memoized version of {@link core.stringToPath}.<br />
+ * The cache has a maximum size of 1000, when overflowing the cache is cleared.
+ * @param {string} str The string to convert
+ * @return {(string|number)[]} The path represented as an array of keys
+ * @memberof core
+ * @private
+ * @since 0.4.0
+ */
 const memoizedStringToPath = str => {
   if (cache.has(str)) return cache.get(str)
 
@@ -122,8 +188,26 @@ const memoizedStringToPath = str => {
   return path
 }
 
-const toPath = allowingArrays(str => [...memoizedStringToPath(str)])
+/**
+ * Converts <code>arg</code> to a path represented as an array of keys.<br />
+ * <code>arg</code> may be a string, in which case it will be parsed.<br />
+ * It may also be an Array, in which case a copy of the array with values converted to path keys will be returned.<br />
+ * If <code>arg</code> is neither a string nor an Array, its string representation will be parsed.
+ * @param {string|Array|*} arg The value to convert
+ * @return {(string|number)[]} The path represented as an array of keys
+ * @memberof core
+ * @since 0.4.0
+ */
+const toPath = allowingArrays(arg => [...memoizedStringToPath(arg)])
 
+/**
+ * This method is like {@link core.toPath} except it returns memoized arrays which must not be mutated.
+ * @param {string|Array|*} arg The value to convert
+ * @return {(string|number)[]} The path represented as an array of keys
+ * @memberof core
+ * @since 0.4.0
+ * @private
+ */
 const unsafeToPath = allowingArrays(memoizedStringToPath)
 
 export { toPath, unsafeToPath }
--- src/util/lang.js
+++ src/util/lang.js
@@ -1,8 +1,36 @@
+/**
+ * Tests whether <code>arg</code> is a valid index, that is a positive integer.
+ * @param {*} arg The value to test
+ * @return {boolean} True if <code>arg</code> is a valid index, false otherwise
+ * @memberof util
+ * @private
+ * @since 0.4.0
+ */
+const isIndex = arg => Number.isSafeInteger(arg) && arg >= 0
+
+/**
+ * Tests whether <code>arg</code> is a Symbol.
+ * @param {*} arg The value to test
+ * @return {boolean} True if <code>arg</code> is a Symbol, false otherwise
+ * @memberof util
+ * @private
+ * @since 0.4.0
+ * @see {@link https://mdn.io/Symbol|Symbol} for more information.
+ */
 const isSymbol = arg => typeof arg === 'symbol'
 
+/**
+ * Converts <code>arg</code> to a string using string interpolation.
+ * @param {*} arg The value to convert
+ * @return {string} The string representation of <code>arg</code>
+ * @memberof util
+ * @private
+ * @since 0.4.0
+ */
 const toString = arg => typeof arg === 'string' ? arg : `${arg}`
 
 export {
+  isIndex,
   isSymbol,
   toString,
 }
