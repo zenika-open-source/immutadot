aaa477ebc1df96bb1a13177864eeebfca962d5d5
Hugo Wood <hx4.5@free.fr>
2017-09-18 14:29:18 +0200

    recursive toPath

--- packages/immutadot/src/core/toPath.js
+++ packages/immutadot/src/core/toPath.js
@@ -112,139 +112,106 @@ const allowingArrays = fn => arg => {
  * @since 1.0.0
  */
 const stringToPath = str => {
-  const path = []
-  let index = 0
+  const path = stringToPath2(str)
+  return str[0] === '.' ? ['', ...path] : path
+}
 
-  while (true) { // eslint-disable-line no-constant-condition
-    // Look for new dot or opening square bracket
-    const nextPointIndex = str.indexOf('.', index)
-    const nextBracketIndex = str.indexOf('[', index)
+const stringToPath2 = str => {
+  // Stop if end of string has been reached
+  if (str.length === 0) return []
 
-    // If neither one is found add the end of str to the path and stop
-    if (nextPointIndex === -1 && nextBracketIndex === -1) {
-      path.push(str.substring(index))
-      break
-    }
+  // Look for new dot or opening square bracket
+  const nextPoint = str.indexOf('.')
+  const nextBracket = str.indexOf('[')
 
-    let isArrayNotation = false
+  // If neither one is found add the end of str to the path and stop
+  if (nextPoint === -1 && nextBracket === -1)
+    return [str]
 
-    // If a dot is found before an opening square bracket
-    if (nextPointIndex !== -1 && (nextBracketIndex === -1 || nextPointIndex < nextBracketIndex)) {
-      // Add the text preceding the dot to the path and move index after the dot
-      path.push(str.substring(index, nextPointIndex))
-      index = nextPointIndex + 1
+  // If a dot is found before an opening square bracket
+  if (nextPoint !== -1 && (nextBracket === -1 || nextPoint < nextBracket))
+  // Add the text preceding the dot to the path and move index after the dot
+    return [str.substring(0, nextPoint), ...stringToPath2(str.substring(nextPoint + 1))]
 
-      // If an opening square bracket follows the dot,
-      // enable array notation and move index after the bracket
-      if (nextBracketIndex === nextPointIndex + 1) {
-        isArrayNotation = true
-        index = nextBracketIndex + 1
-      }
+  // If an opening square bracket is found before a dot
+  else if (nextBracket > 0)
+    // If any text precedes the bracket, add it to the path
+    return [str.substring(0, nextBracket), ...stringToPath2(str.substring(nextBracket))]
 
-    // If an opening square bracket is found before a dot
-    } else if (nextBracketIndex !== -1) {
-      // Enable array notation
-      isArrayNotation = true
+  // If an square bracket is found in head position
 
-      // If any text precedes the bracket, add it to the path
-      if (nextBracketIndex !== index)
-        path.push(str.substring(index, nextBracketIndex))
+  // Check if next character is a quote
+  const { quoted, quote } = isQuoteChar(str, 1)
 
-      // Move index after the bracket
-      index = nextBracketIndex + 1
-    }
+  // If array index is a quoted string
+  if (quoted) {
+    // Look for the next unescaped matching quote
+    let endQuoteIndex, quotedIndex = 2
+    do {
+      endQuoteIndex = str.indexOf(quote, quotedIndex)
+      quotedIndex = endQuoteIndex + 1
+    } while (endQuoteIndex !== -1 && str.charAt(endQuoteIndex - 1) === '\\')
 
-    // If array notation is enabled
-    if (isArrayNotation) {
-      // Check if next character is a string quote
-      const { quoted, quote } = isQuoteChar(str, index)
+    // If no end delimiter found, stop if end of str is reached, or continue to next iteration
+    if (endQuoteIndex === -1)
+      return str.length > 2 ? [str.substring(2)] : []
 
-      // If array index is a quoted string
-      if (quoted) {
-        // Move index after the string quote
-        index++
+      // Move index after end delimiter
+    let index = endQuoteIndex + 1
 
-        // Look for the next unescaped matching string quote
-        let endQuoteIndex, quotedIndex = index
-        do {
-          endQuoteIndex = str.indexOf(quote, quotedIndex)
-          quotedIndex = endQuoteIndex + 1
-        } while (endQuoteIndex !== -1 && str.charAt(endQuoteIndex - 1) === '\\')
+    // If next character is a closing square bracket, move index after it
+    if (str.charAt(index) === ']') index++
 
-        // If no end quote found, stop if end of str is reached, or continue to next iteration
-        if (endQuoteIndex === -1) {
-          if (index !== str.length) path.push(str.substring(index))
-          break
-        }
+    // Stop if end of str has been reached
+    // if (index === str.length) break
 
-        // Add the content of quotes to the path, unescaping escaped quotes
-        path.push(unescapeQuotes(str.substring(index, endQuoteIndex), quote))
+    // If next character is a dot, move index after it (skip it)
+    if (str.charAt(index) === '.') index++
 
-        // Move index after end quote
-        index = endQuoteIndex + 1
-
-        // If next character is a closing square bracket, move index after it
-        if (str.charAt(index) === ']') index++
-
-        // Stop if end of str has been reached
-        if (index === str.length) break
-
-        // If next character is a dot, move index after it (skip it)
-        if (str.charAt(index) === '.') index++
-
-      } else { // If array index is not a quoted string
-
-        // Look for the closing square bracket
-        const closingBracketIndex = str.indexOf(']', index)
-
-        // If no closing bracket found, stop if end of str is reached, or continue to next iteration
-        if (closingBracketIndex === -1) {
-          if (index !== str.length) path.push(str.substring(index))
-          break
-        }
-
-        // Fetch the content of brackets and move index after closing bracket
-        const arrayIndexValue = str.substring(index, closingBracketIndex)
-        index = closingBracketIndex + 1
-
-        // If next character is a dot, move index after it (skip it)
-        if (str.charAt(index) === '.') index++
-
-        // Shorthand: if array index is the whole slice add it to path
-        if (arrayIndexValue === ':') {
-          path.push([undefined, undefined])
-        } else {
-
-          // Look for a slice quote
-          const sliceDelimIndex = arrayIndexValue.indexOf(':')
-
-          // If no slice quote found
-          if (sliceDelimIndex === -1) {
-            // Parse array index as a number
-            const nArrayIndexValue = Number(arrayIndexValue)
-
-            // Add array index to path, either as a valid index (positive int), or as a string
-            path.push(isIndex(nArrayIndexValue) ? nArrayIndexValue : arrayIndexValue)
-
-          } else { // If a slice quote is found
-
-            // Fetch slice start and end, and parse them as slice indexes (empty or valid int)
-            const sliceStart = arrayIndexValue.substring(0, sliceDelimIndex), sliceEnd = arrayIndexValue.substring(sliceDelimIndex + 1)
-            const nSliceStart = toSliceIndex(sliceStart), nSliceEnd = toSliceIndex(sliceEnd)
-
-            // Add array index to path, as a slice if both slice indexes are valid (undefined or int), or as a string
-            path.push(isSliceIndex(nSliceStart) && isSliceIndex(nSliceEnd) ? [nSliceStart, nSliceEnd] : arrayIndexValue)
-          }
-        }
-
-        // Stop if end of string has been reached
-        if (index === str.length) break
-      }
-    }
+    // Add the content of delimiters to the path, unescaping escaped delimiters
+    return [unescapeQuotes(str.substring(2, endQuoteIndex), quote), ...stringToPath2(str.substring(index))]
 
   }
+  // If array index is not a delimited string
 
-  return path
+  // Look for the closing square bracket
+  const closingBracket = str.indexOf(']')
+
+  // If no closing bracket found, stop if end of str is reached, or continue to next iteration
+  if (closingBracket === -1)
+    return str.length > 1 ? [str.substring(1)] : []
+
+  // Fetch the content of brackets and move index after closing bracket
+  const arrayIndex = str.substring(1, closingBracket)
+
+  let index = closingBracket + 1
+
+  // If next character is a dot, move index after it (skip it)
+  if (str.charAt(index) === '.') index++
+
+  // Shorthand: if array index is the whole slice add it to path
+  if (arrayIndex === ':')
+    return [[undefined, undefined], ...stringToPath2(str.substring(index))]
+
+    // Look for a slice delimiter
+  const sliceDelimIndex = arrayIndex.indexOf(':')
+
+  // If no slice delimiter found
+  if (sliceDelimIndex === -1) {
+    // Parse array index as a number
+    const nArrayIndex = Number(arrayIndex)
+
+    // Add array index to path, either as a valid index (positive int), or as a string
+    return [isIndex(nArrayIndex) ? nArrayIndex : arrayIndex, ...stringToPath2(str.substring(index))]
+
+  } // If a slice delimiter is found
+
+  // Fetch slice start and end, and parse them as slice indexes (empty or valid int)
+  const sliceStart = arrayIndex.substring(0, sliceDelimIndex), sliceEnd = arrayIndex.substring(sliceDelimIndex + 1)
+  const nSliceStart = toSliceIndex(sliceStart), nSliceEnd = toSliceIndex(sliceEnd)
+
+  // Add array index to path, as a slice if both slice indexes are valid (undefined or int), or as a string
+  return [isSliceIndex(nSliceStart) && isSliceIndex(nSliceEnd) ? [nSliceStart, nSliceEnd] : arrayIndex, ...stringToPath2(str.substring(index))]
 }
 
 const MAX_CACHE_SIZE = 1000
