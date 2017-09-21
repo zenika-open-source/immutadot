e003363714afaed27e7b097e1d5d41e3e482bc67
Hugo Wood <hx4.5@free.fr>
2017-09-21 16:04:12 +0200

    :hammer: smaller functions

--- packages/immutadot/src/core/toPath.js
+++ packages/immutadot/src/core/toPath.js
@@ -116,72 +116,51 @@ const stringToPath = str => {
   return str[0] === '.' ? ['', ...path] : path
 }
 
-const stringToPath2 = str => {
-  // Stop if end of string has been reached
-  if (str.length === 0) return []
+const splitAtFirstOccurence = (str, separators) => {
+  const partitionIndex = separators
+    .map(separator => str.indexOf(separator))
+    .map(index => index >= 0 ? index : str.length)
+    .reduce((minIndex, index) => Math.min(minIndex, index))
+  return [str.substring(0, partitionIndex), str.substr(partitionIndex, 1), str.substring(partitionIndex + 1)]
+}
 
-  // Look for new dot or opening square bracket
-  const nextPoint = str.indexOf('.')
-  const nextBracket = str.indexOf('[')
+const parseQuotedBracketNotation = (str, quote) => {
+  // Look for the next unescaped matching quote
+  let endQuoteIndex, quotedIndex = 2
+  do {
+    endQuoteIndex = str.indexOf(quote, quotedIndex)
+    quotedIndex = endQuoteIndex + 1
+  } while (endQuoteIndex !== -1 && str.charAt(endQuoteIndex - 1) === '\\')
 
-  // If neither one is found add the end of str to the path and stop
-  if (nextPoint === -1 && nextBracket === -1)
-    return [str]
+  // If no end delimiter found, stop if end of str is reached, or continue to next iteration
+  if (endQuoteIndex === -1)
+    return str.length > 2 ? [str.substring(2), ''] : [undefined, '']
 
-  // If a dot is found before an opening square bracket
-  if (nextPoint !== -1 && (nextBracket === -1 || nextPoint < nextBracket))
-  // Add the text preceding the dot to the path and move index after the dot
-    return [str.substring(0, nextPoint), ...stringToPath2(str.substring(nextPoint + 1))]
+    // Move index after end delimiter
+  let index = endQuoteIndex + 1
 
-  // If an opening square bracket is found before a dot
-  else if (nextBracket > 0)
-    // If any text precedes the bracket, add it to the path
-    return [str.substring(0, nextBracket), ...stringToPath2(str.substring(nextBracket))]
+  // If next character is a closing square bracket, move index after it
+  if (str.charAt(index) === ']') index++
 
-  // If an square bracket is found in head position
+  // Stop if end of str has been reached
+  // if (index === str.length) break
 
-  // Check if next character is a quote
-  const { quoted, quote } = isQuoteChar(str, 1)
+  // If next character is a dot, move index after it (skip it)
+  if (str.charAt(index) === '.') index++
 
-  // If array index is a quoted string
-  if (quoted) {
-    // Look for the next unescaped matching quote
-    let endQuoteIndex, quotedIndex = 2
-    do {
-      endQuoteIndex = str.indexOf(quote, quotedIndex)
-      quotedIndex = endQuoteIndex + 1
-    } while (endQuoteIndex !== -1 && str.charAt(endQuoteIndex - 1) === '\\')
+  // Add the content of delimiters to the path, unescaping escaped delimiters
+  return [unescapeQuotes(str.substring(2, endQuoteIndex), quote), str.substring(index)]
+}
 
-    // If no end delimiter found, stop if end of str is reached, or continue to next iteration
-    if (endQuoteIndex === -1)
-      return str.length > 2 ? [str.substring(2)] : []
-
-      // Move index after end delimiter
-    let index = endQuoteIndex + 1
-
-    // If next character is a closing square bracket, move index after it
-    if (str.charAt(index) === ']') index++
-
-    // Stop if end of str has been reached
-    // if (index === str.length) break
-
-    // If next character is a dot, move index after it (skip it)
-    if (str.charAt(index) === '.') index++
-
-    // Add the content of delimiters to the path, unescaping escaped delimiters
-    return [unescapeQuotes(str.substring(2, endQuoteIndex), quote), ...stringToPath2(str.substring(index))]
-
-  }
-  // If array index is not a delimited string
-
+const parseBareBracketNotation = str => {
   // Look for the closing square bracket
   const closingBracket = str.indexOf(']')
 
   // If no closing bracket found, stop if end of str is reached, or continue to next iteration
   if (closingBracket === -1)
-    return str.length > 1 ? [str.substring(1)] : []
+    return str.length > 1 ? [str.substring(1), ''] : [undefined, '']
 
-  // Fetch the content of brackets and move index after closing bracket
+    // Fetch the content of brackets and move index after closing bracket
   const arrayIndex = str.substring(1, closingBracket)
 
   let index = closingBracket + 1
@@ -191,9 +170,9 @@ const stringToPath2 = str => {
 
   // Shorthand: if array index is the whole slice add it to path
   if (arrayIndex === ':')
-    return [[undefined, undefined], ...stringToPath2(str.substring(index))]
+    return [[undefined, undefined], str.substring(index)]
 
-    // Look for a slice delimiter
+  // Look for a slice delimiter
   const sliceDelimIndex = arrayIndex.indexOf(':')
 
   // If no slice delimiter found
@@ -202,7 +181,7 @@ const stringToPath2 = str => {
     const nArrayIndex = Number(arrayIndex)
 
     // Add array index to path, either as a valid index (positive int), or as a string
-    return [isIndex(nArrayIndex) ? nArrayIndex : arrayIndex, ...stringToPath2(str.substring(index))]
+    return [isIndex(nArrayIndex) ? nArrayIndex : arrayIndex, str.substring(index)]
 
   } // If a slice delimiter is found
 
@@ -211,7 +190,30 @@ const stringToPath2 = str => {
   const nSliceStart = toSliceIndex(sliceStart), nSliceEnd = toSliceIndex(sliceEnd)
 
   // Add array index to path, as a slice if both slice indexes are valid (undefined or int), or as a string
-  return [isSliceIndex(nSliceStart) && isSliceIndex(nSliceEnd) ? [nSliceStart, nSliceEnd] : arrayIndex, ...stringToPath2(str.substring(index))]
+  return [isSliceIndex(nSliceStart) && isSliceIndex(nSliceEnd) ? [nSliceStart, nSliceEnd] : arrayIndex, str.substring(index)]
+}
+
+const parseBracketNotation = str => {
+  const { quoted, quote } = isQuoteChar(str, 1)
+  if (quoted)
+    return parseQuotedBracketNotation(str, quote)
+  return parseBareBracketNotation(str)
+}
+
+const stringToPath2 = str => {
+  if (str.length === 0)
+    return []
+  if (str[0] === '[') {
+    const [bracketedPathSegment, rest] = parseBracketNotation(str)
+    const restOfPath = stringToPath2(rest)
+    return bracketedPathSegment !== undefined
+      ? [bracketedPathSegment, ...restOfPath]
+      : [...restOfPath]
+  }
+  const [beforeSeparator, separator, afterSeparator] = splitAtFirstOccurence(str, ['.', '['])
+  return separator === '.'
+    ? [beforeSeparator, ...stringToPath2(afterSeparator)]
+    : [beforeSeparator, ...stringToPath2(separator + afterSeparator)]
 }
 
 const MAX_CACHE_SIZE = 1000
