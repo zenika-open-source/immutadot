6112bc9dd7a9de268ca5a04db3fa19e27886c0c8
nlepage <19571875+nlepage@users.noreply.github.com>
2017-11-28 00:38:03 +0100

    :wrench: Use jest coverage ignores...

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 const { resolve } = require('path')
 
 const coverageDirectory = resolve(process.cwd(), 'coverage')
+const coveragePathIgnorePatterns = ['<rootDir>/misc/', '/node_modules/']
 const roots = [process.cwd()]
 const setupTestFrameworkScriptFile = resolve(__dirname, 'misc/test.setup.js')
 
 module.exports = {
   coverageDirectory,
+  coveragePathIgnorePatterns,
   coverageReporters: ['lcov'],
   roots,
   setupTestFrameworkScriptFile,
