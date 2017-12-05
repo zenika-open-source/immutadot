fc2d6578ec3f04596132f3130f6ff36c315e3f6e
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-05 18:01:32 +0100

    🚧 flow: Send appliedPaths to applier

--- packages/immutadot/src/flow/flow.js
+++ packages/immutadot/src/flow/flow.js
@@ -7,6 +7,11 @@
  * @since 1.0.0
  */
 
+const flowReduceCallback = ([obj, appliedPaths], fn) => [
+  fn(obj, appliedPaths),
+  [...appliedPaths, fn.path],
+]
+
 /**
  * Successively calls <code>fns</code>.<br/>
  * Each function is called with the result of the previous one.
@@ -16,6 +21,12 @@
  * @returns {flow.flowFunction} A function successively calling <code>fns</code>
  * @since 1.0.0
  */
-const flow = (...fns) => pObj => fns.reduce((obj, fn) => fn(obj), pObj)
+const flow = (...fns) => obj => {
+  const [result] = fns.reduce(
+    flowReduceCallback,
+    [obj, []],
+  )
+  return result
+}
 
 export { flow }
