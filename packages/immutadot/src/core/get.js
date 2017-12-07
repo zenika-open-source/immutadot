a3232f3e400b56286cf461ecd2558de324364134
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-07 12:27:07 +0100

    💡 core.get documentation

--- packages/immutadot/src/core/get.js
+++ packages/immutadot/src/core/get.js
@@ -1,13 +1,23 @@
 import { isNil } from 'util/lang'
 import { unsafeToPath } from './toPath'
 
-export function get(obj, pPath, defaultValue) {
-  const path = unsafeToPath(pPath)
-  const walkPath = (curObj, remPath) => {
+/**
+* Gets the value at <code>path</code> of <code>obj</code>.
+* @memberof core
+* @param {*} obj The object.
+* @param {string|Array} path The path of the property to get.
+* @param {*} defaultValue The default value.
+* @return {*} Returns the value or <code>defaultValue</code>.
+* @example get({ nested: { prop: 'val' } }, 'nested.prop') // => 'val'
+* @example get({ nested: { prop: 'val' } }, 'nested.unknown', 'default') // => 'default'
+* @since 1.0.0
+ */
+export function get(obj, path, defaultValue) {
+  function walkPath(curObj, remPath) {
     if (remPath.length === 0) return curObj === undefined ? defaultValue : curObj
     if (isNil(curObj)) return defaultValue
     const [prop, ...pathRest] = remPath
     return walkPath(curObj[prop], pathRest)
   }
-  return walkPath(obj, path)
+  return walkPath(obj, unsafeToPath(path))
 }
