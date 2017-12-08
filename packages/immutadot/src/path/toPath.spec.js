092a96e57fd164d15ad6c3835a1c18dd4ed7fd73
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-08 10:46:11 +0100

    :white_check_mark: Update toPath tests for list notation

--- packages/immutadot/src/path/toPath.spec.js
+++ packages/immutadot/src/path/toPath.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import {
   index,
+  list,
   prop,
   slice,
 } from './consts'
@@ -39,9 +40,14 @@ describe('ToPath', () => {
     expect(toPath('[1:2:3][1:a][1:2')).toEqual([[prop, '1:2:3'], [prop, '1:a'], [prop, '1:2']])
   })
 
+  it('should convert list notation path', () => {
+    expect(toPath('{abc,defg}.{123,4567,89},{foo}')).toEqual([[list, ['abc', 'defg']], [list, ['123', '4567', '89']], [prop, 'foo']])
+    expect(toPath('{"abc,defg",foo}.{\'123,4567,89\'}')).toEqual([[list, ['abc,defg', 'foo']], [prop, '123,4567,89']])
+  })
+
   it('should convert mixed path', () => {
-    expect(toPath('a[0]["b.c"].666[1:]')).toEqual([[prop, 'a'], [index, 0], [prop, 'b.c'], [prop, '666'], [slice, [1, undefined]]])
-    expect(toPath('a.[0].["b.c"]666[1:2:3]')).toEqual([[prop, 'a'], [index, 0], [prop, 'b.c'], [prop, '666'], [prop, '1:2:3']])
+    expect(toPath('a[0]["b.c"].666[1:].{1a,2b,3c}')).toEqual([[prop, 'a'], [index, 0], [prop, 'b.c'], [prop, '666'], [slice, [1, undefined]], [list, ['1a', '2b', '3c']]])
+    expect(toPath('a.[0].["b.c"]666[1:2:3]{1a}{"2b",\'3c\'}')).toEqual([[prop, 'a'], [index, 0], [prop, 'b.c'], [prop, '666'], [prop, '1:2:3'], [prop, '1a'], [list, ['2b', '3c']]])
   })
 
   it('should not convert array path', () => {
@@ -51,12 +57,14 @@ describe('ToPath', () => {
       [prop, 'test'],
       [slice, [1, undefined]],
       [slice, [0, -2]],
+      [list, ['1a', '2b', '3c']],
     ])).toEqual([
       [index, 666],
       [prop, Symbol.for('🍺')],
       [prop, 'test'],
       [slice, [1, undefined]],
       [slice, [0, -2]],
+      [list, ['1a', '2b', '3c']],
     ])
   })
 
