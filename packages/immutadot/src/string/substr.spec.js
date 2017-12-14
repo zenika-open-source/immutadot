34fec30ae36b3ae67d3c29d6ed95cc3f373cf164
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-14 14:39:52 +0100

    :sparkles: Add string.substr

--- packages/immutadot/src/string/substr.spec.js
+++ packages/immutadot/src/string/substr.spec.js
@@ -1,12 +1,12 @@
 /* eslint-env jest */
 import { immutaTest } from 'test.utils'
-import { slice } from 'string'
+import { substr } from 'string'
 
-describe('string.slice', () => {
+describe('string.substr', () => {
 
   it('should return a slice to the end', () => {
     immutaTest((input, path) => {
-      const output = slice(input, path, 6)
+      const output = substr(input, path, 6)
       expect(output).toEqual({ nested: { prop: 'World !' } })
       return output
     }, { nested: { prop: 'Hello World !' } }, 'nested.prop')
@@ -14,7 +14,7 @@ describe('string.slice', () => {
 
   it('should return a slice', () => {
     immutaTest((input, path) => {
-      const output = slice(input, path, 6, 11)
+      const output = substr(input, path, 6, 5)
       expect(output).toEqual({ nested: { prop: 'World' } })
       return output
     }, { nested: { prop: 'Hello World !' } }, 'nested.prop')
