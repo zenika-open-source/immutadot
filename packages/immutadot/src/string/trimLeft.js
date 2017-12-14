1234b26b9cc4bdb655844ab93ef618d1929ee45d
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-14 15:22:15 +0100

    :sparkles: Add string.trimLeft and string.trimRight

---
+++ packages/immutadot/src/string/trimLeft.js
@@ -0,0 +1,17 @@
+import { convertStringMethod } from './convertStringMethod'
+
+/**
+ * Replaces by former string stripped of whitespaces at start.
+ * @function
+ * @memberof string
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @return {Object} Returns the updated object.
+ * @example trimLeft({ nested: { a: '   Hello world !   ' } }) // => { nested: { a: 'Hello world !   ' } }
+ * @see {@link https://mdn.io/String.prototype.trimLeft|String.prototype.trimLeft} for more information.
+ * @since 1.0.0
+ * @flow
+ */
+const trimLeft = convertStringMethod('trimLeft')
+
+export { trimLeft }
