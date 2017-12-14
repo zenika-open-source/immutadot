14cfdab0018adbcd5d08f4280dea0b1075858790
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-14 14:01:55 +0100

    :sparkles: Add string.slice

---
+++ packages/immutadot/src/string/slice.js
@@ -0,0 +1,20 @@
+import { convertStringMethod } from './convertStringMethod'
+
+/**
+ * Replaces by a slice of former string starting at <code>beginIndex</code> and ending at <code>endIndex</code> or end of the string.
+ * @function
+ * @memberof string
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {number} beginIndex Beginning index of slice.
+ * @param {number?} endIndex Ending index of slice.
+ * @return {Object} Returns the updated object.
+ * @example slice({ nested: { a: 'Hello World !' } }, 6) // => { nested: { a: 'World !' } }
+ * @example slice({ nested: { a: 'Hello World !' } }, 6, 11) // => { nested: { a: 'World' } }
+ * @see {@link https://mdn.io/String.prototype.slice|String.prototype.slice} for more information.
+ * @since 1.0.0
+ * @flow
+ */
+const slice = convertStringMethod('slice')
+
+export { slice }
---
+++ packages/immutadot/src/string/slice.spec.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+import { immutaTest } from 'test.utils'
+import { slice } from 'string'
+
+describe('string.slice', () => {
+
+  it('should return a slice to the end', () => {
+    immutaTest((input, path) => {
+      const output = slice(input, path, 6)
+      expect(output).toEqual({ nested: { prop: 'World !' } })
+      return output
+    }, { nested: { prop: 'Hello World !' } }, 'nested.prop')
+  })
+
+  it('should return a slice to the end', () => {
+    immutaTest((input, path) => {
+      const output = slice(input, path, 6, 11)
+      expect(output).toEqual({ nested: { prop: 'World' } })
+      return output
+    }, { nested: { prop: 'Hello World !' } }, 'nested.prop')
+  })
+})
