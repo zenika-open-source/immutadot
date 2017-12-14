e7764ca57164efbd574ae0351b25de78df09db3e
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-14 14:45:53 +0100

    :sparkles: Add string.substring

--- packages/immutadot/src/string/substr.js
+++ packages/immutadot/src/string/substr.js
@@ -1,16 +1,16 @@
 import { convertStringMethod } from './convertStringMethod'
 
 /**
- * Replaces by a slice of former string starting at <code>beginIndex</code> and ending at <code>endIndex</code> or end of the string.
+ * Replaces by a slice of former string starting at <code>beginIndex</code>.
  * @function
  * @memberof string
  * @param {Object} object The object to modify.
  * @param {Array|string} path The path of the property to set.
  * @param {number} beginIndex Beginning index of slice.
- * @param {number?} endIndex Ending index of slice.
+ * @param {number?} length Length of slice.
  * @return {Object} Returns the updated object.
  * @example substr({ nested: { a: 'Hello World !' } }, 6) // => { nested: { a: 'World !' } }
- * @example substr({ nested: { a: 'Hello World !' } }, 6, 11) // => { nested: { a: 'World' } }
+ * @example substr({ nested: { a: 'Hello World !' } }, 6, 5) // => { nested: { a: 'World' } }
  * @see {@link https://mdn.io/String.prototype.substr|String.prototype.substr} for more information.
  * @since 1.0.0
  * @flow
