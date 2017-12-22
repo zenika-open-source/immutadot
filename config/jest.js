3957872591118661dcdc6a0825ead4f9aeaa0dae
nlepage <19571875+nlepage@users.noreply.github.com>
2017-12-22 11:04:02 +0100

    :wrench: fix generate flow and coverage config

--- config/jest.js
+++ config/jest.js
@@ -3,14 +3,11 @@ const { resolve } = require('path')
 const rootDir = process.cwd()
 
 const coverageDirectory = resolve(rootDir, 'coverage')
-const coveragePathIgnorePatterns = ['<rootDir>/misc/', '/node_modules/']
-const roots = [rootDir]
 const setupTestFrameworkScriptFile = resolve(__dirname, 'test.setup.js')
 
 module.exports = {
   coverageDirectory,
-  coveragePathIgnorePatterns,
   coverageReporters: ['lcov'],
-  roots,
+  rootDir,
   setupTestFrameworkScriptFile,
 }
