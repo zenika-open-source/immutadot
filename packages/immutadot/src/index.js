b30292373534c8cc6e0a09ff53956efbab4e4cd1
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-12-24 10:54:37 +0100

    :fire: Remove copy paste in wrong file...

--- packages/immutadot/src/index.js
+++ packages/immutadot/src/index.js
@@ -5,18 +5,3 @@ export * from './object'
 export * from './seq'
 export * from './string'
 export * from './util'
-
-/*
-{
-  weasel: [
-    {
-      vernacularName: 'Badger',
-      scientificName: 'Meles meles'
-    },
-    {
-      vernacularName: 'Otter',
-      scientificName: 'Lutrinae'
-    }
-  ]
-}
-*/
