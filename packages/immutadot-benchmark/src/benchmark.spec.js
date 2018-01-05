42ba3134ca0ef01da54d0e17c5dd85d463a91654
nlepage <19571875+nlepage@users.noreply.github.com>
2018-01-05 02:23:48 +0100

    :recycle: Benchmark with iterations

--- packages/immutadot-benchmark/src/benchmark.spec.js
+++ packages/immutadot-benchmark/src/benchmark.spec.js
@@ -1,26 +1,57 @@
 /* eslint-env jest */
-'use strict'
-
-// Initial source: https://github.com/mweststrate/immer/blob/2a1da31f8d0408857697c04f6a683fd955ab7a06/__tests__/performance.js
 
 import { List, Record } from 'immutable'
 
 import immer, { setAutoFreeze } from 'immer'
-import immerES5, { setAutoFreeze as setAutoFreezeES5 } from 'immer'
+import immerES5, { setAutoFreeze as setAutoFreezeES5 } from 'immer/es5'
 
 import cloneDeep from 'lodash/cloneDeep'
 
 import { set } from 'immutadot/core'
 
+function benchmark(title, operation, maxTime = 30, maxOperations = 1000) {
+  const startTime = Date.now()
+  const maxTimeMs = maxTime * 1000
+  const maxRunTime = maxTimeMs / 10 // Max run time is a tenth of max time
+  const limitEndTime = startTime + maxTimeMs
+
+  let iterations = Math.min(10, maxOperations) // Start 10 iterations
+  let nbOperations = 0
+  let totalTime = 0
+
+  while (iterations > 0) {
+    nbOperations += iterations
+
+    const runStartTime = Date.now()
+    while (iterations--) operation()
+    totalTime += Date.now() - runStartTime
+
+    const tempMeanTime = totalTime / nbOperations
+
+    iterations = Math.min(
+      // Either enough operations to consume max run time or remaining time
+      Math.ceil(Math.min(maxRunTime, Math.max(limitEndTime - Date.now(), 0)) / tempMeanTime),
+      // Or enough operations to reach max operations
+      maxOperations - nbOperations,
+    )
+  }
+
+  return {
+    title,
+    totalTime,
+    nbOperations,
+  }
+}
+
 describe('Benchmarks', () => {
 
   describe('Update todos list', () => {
-    const MAX = 100000
-    const MODIFY_FACTOR = 0.1
+    const listSize = 100000
+    const modifySize = 10000
 
     // Prepare base state
     const baseState = []
-    for (let i = 0; i < MAX; i++) {
+    for (let i = 0; i < listSize; i++) {
       baseState.push({
         todo: `todo_${i}`,
         done: false,
@@ -28,101 +59,96 @@ describe('Benchmarks', () => {
       })
     }
 
-    // Prepare frozen state
-    const frozenState = Object.freeze(baseState.map(todo => Object.freeze({
-      ...todo,
-      someThingCompletelyIrrelevant: Object.freeze([...todo.someThingCompletelyIrrelevant]),
-    })))
-
     // Prepare immutalbe state
     const todoRecord = Record({
       todo: '',
       done: false,
       someThingCompletelyIrrelevant: [],
     })
-    const immutableState = List(frozenState.map(todo => todoRecord(todo)))
+    const immutableState = List(baseState.map(todo => todoRecord(todo)))
 
     // Disable immer auto freeze
     setAutoFreeze(false)
     setAutoFreezeES5(false)
 
-    // Prepare expectd state
-    const expectedState = []
-    for (let i = 0; i < MAX; i++) {
-      expectedState.push({
-        todo: `todo_${i}`,
-        done: i < MAX * MODIFY_FACTOR,
-        someThingCompletelyIrrelevant: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-      })
-    }
+    const results = []
 
-    it('with mutation', () => {
-      for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-        baseState[i].done = true
+    // FIXME add lodash to dependencies
+    it('deep cloning', () => {
+      results.push(benchmark('lodash deep cloning, then mutation', () => {
+        const newState = cloneDeep(baseState)
+        for (let i = 0; i < modifySize; i++)
+          newState[i].done = true
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let deepCloneActual
-    it('with deep cloning, then mutation', () => {
-      deepCloneActual = cloneDeep(frozenState)
-      for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-        deepCloneActual[i].done = true
+    it('ES2015', () => {
+      results.push(benchmark('ES2015 destructuring', () => {
+        const newState = baseState
+          .slice(0, modifySize)
+          .map(todo => ({
+            ...todo,
+            done: true,
+          }))
+          .concat(baseState.slice(modifySize))
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let es2015Actual
-    it('with ES2015 destructuring', () => {
-      es2015Actual = frozenState
-        .slice(0, MAX * MODIFY_FACTOR)
-        .map(todo => ({
-          ...todo,
-          done: true,
-        }))
-        .concat(frozenState.slice(MAX * MODIFY_FACTOR))
+    it('immutable w/o conversion', () => {
+      results.push(benchmark('immutable (w/o conversion to plain JS objects)', () => {
+        const newState = immutableState.withMutations(state => {
+          for (let i = 0; i < modifySize; i++)
+            state.setIn([i, 'done'], true)
+        })
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    it('with immutable (w/o conversion to plain JS objects)', () => {
-      immutableState.withMutations(state => {
-        for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-          state.setIn([i, 'done'], true)
-      })
+    it('immutable w/ conversion', () => {
+      results.push(benchmark('immutable (w/ conversion to plain JS objects)', () => {
+        const newState = immutableState.withMutations(state => {
+          for (let i = 0; i < modifySize; i++)
+            state.setIn([i, 'done'], true)
+        }).toJS()
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let immutableActual
-    it('with immutable (w/ conversion to plain JS objects)', () => {
-      immutableActual = immutableState.withMutations(state => {
-        for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-          state.setIn([i, 'done'], true)
-      }).toJS()
+    it('immer proxy', () => {
+      results.push(benchmark('immer (proxy implementation w/o autofreeze)', () => {
+        const newState = immer(baseState, draft => {
+          for (let i = 0; i < modifySize; i++)
+            draft[i].done = true
+        })
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let immerActual
-    it('with immer (proxy implementation w/o autofreeze)', () => {
-      immerActual = immer(frozenState, draft => {
-        for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-          draft[i].done = true
-      })
+    it('immer ES5', () => {
+      results.push(benchmark('immer (ES5 implementation w/o autofreeze)', () => {
+        const newState = immerES5(baseState, draft => {
+          for (let i = 0; i < modifySize; i++)
+            draft[i].done = true
+        })
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let immerES5Actual
-    it('with immer (ES5 implementation w/o autofreeze)', () => {
-      immerES5Actual = immerES5(frozenState, draft => {
-        for (let i = 0; i < MAX * MODIFY_FACTOR; i++)
-          draft[i].done = true
-      })
+    it('immutad●t', () => {
+      results.push(benchmark('immutad●t', () => {
+        const newState = set(baseState, `[:${modifySize}].done`, true)
+        expect(newState).not.toBeUndefined()
+      }))
     })
 
-    let immutadotActual
-    it('with immutad●t', () => {
-      immutadotActual = set(frozenState, `[:${MAX * MODIFY_FACTOR}].done`, true)
-    })
-
-    it('check produced todos lists', () => {
-      expect(baseState).toEqual(expectedState)
-      expect(deepCloneActual).toEqual(expectedState)
-      expect(es2015Actual).toEqual(expectedState)
-      expect(immutableActual).toEqual(expectedState)
-      expect(immerActual).toEqual(expectedState)
-      expect(immerES5Actual).toEqual(expectedState)
-      expect(immutadotActual).toEqual(expectedState)
+    afterAll(() => {
+      console.log( // eslint-disable-line no-console
+        results
+          .map(({ title, totalTime, nbOperations }) => `${title}: ~${(totalTime / nbOperations).toFixed(2)}ms/op on ${nbOperations}ops`)
+          .join('\n'),
+      )
     })
   })
 })
