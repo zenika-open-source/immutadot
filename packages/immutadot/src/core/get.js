94f6cd6e8cf3145ad89c72417d8f7488179f226a
nlepage <19571875+nlepage@users.noreply.github.com>
2018-03-06 15:27:07 +0100

    :sparkles: Support first arg currying in get

--- packages/immutadot/src/core/get.js
+++ packages/immutadot/src/core/get.js
@@ -2,13 +2,16 @@ import {
   index,
   prop,
 } from 'path/consts'
-import { isNil } from 'util/lang'
+import {
+  isNil,
+  isString,
+} from 'util/lang'
 import { toPath } from 'path/toPath'
 
 /**
 * Gets the value at <code>path</code> of <code>obj</code>.
 * @memberof core
-* @param {*} obj The object.
+* @param {*} [obj] The object.
 * @param {string|Array} path The path of the property to get.
 * @param {*} defaultValue The default value.
 * @return {*} Returns the value or <code>defaultValue</code>.
@@ -16,17 +19,25 @@ import { toPath } from 'path/toPath'
 * @example get({ nested: { prop: 'val' } }, 'nested.unknown', 'default') // => 'default'
 * @since 1.0.0
  */
-function get(obj, path, defaultValue) {
-  function walkPath(curObj, remPath) {
-    if (remPath.length === 0) return curObj === undefined ? defaultValue : curObj
-    if (isNil(curObj)) return defaultValue
-    const [[, prop], ...pathRest] = remPath
-    return walkPath(curObj[prop], pathRest)
+function get(...args) {
+  const [firstArg, ...argsRest] = args
+  if (isString(firstArg)) return makeGetter(...args)
+  return makeGetter(...argsRest)(firstArg)
+}
+
+function makeGetter(path, defaultValue) {
+  return function(obj) {
+    function walkPath(curObj, remPath) {
+      if (remPath.length === 0) return curObj === undefined ? defaultValue : curObj
+      if (isNil(curObj)) return defaultValue
+      const [[, prop], ...pathRest] = remPath
+      return walkPath(curObj[prop], pathRest)
+    }
+    const parsedPath = toPath(path)
+    if (parsedPath.some(([propType]) => propType !== prop && propType !== index))
+      throw TypeError('get supports only properties and array indexes in path')
+    return walkPath(obj, parsedPath)
   }
-  const parsedPath = toPath(path)
-  if (parsedPath.some(([propType]) => propType !== prop && propType !== index))
-    throw TypeError('get supports only properties and array indexes in path')
-  return walkPath(obj, parsedPath)
 }
 
 export { get }
