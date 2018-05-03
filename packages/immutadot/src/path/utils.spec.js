ff3a65b892718ce2ab13f61eda97bf8dda9fcc62
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-05-03 10:09:37 +0200

    :twisted_rightwards_arrows: Fix after rebase

--- packages/immutadot/src/path/utils.spec.js
+++ packages/immutadot/src/path/utils.spec.js
@@ -54,30 +54,7 @@ describe('path utils', () => {
       ])
     })
   })
-<<<<<<< HEAD
 
-=======
-  describe('path.isSliceIndex', () => {
-    it('should return true for any integer or undefined', () => {
-      expect(isSliceIndex(0)).toBe(true)
-      expect(isSliceIndex(1)).toBe(true)
-      expect(isSliceIndex(6)).toBe(true)
-      expect(isSliceIndex(100000000000)).toBe(true)
-      expect(isSliceIndex(-1)).toBe(true)
-      expect(isSliceIndex(-6)).toBe(true)
-      expect(isSliceIndex(-100000000000)).toBe(true)
-      expect(isSliceIndex(undefined)).toBe(true)
-    })
-    it('should return false for any non integer except undefined', () => {
-      expect(isSliceIndex(null)).toBe(false)
-      expect(isSliceIndex(true)).toBe(false)
-      expect(isSliceIndex({})).toBe(false)
-      expect(isSliceIndex([])).toBe(false)
-      expect(isSliceIndex('')).toBe(false)
-      expect(isSliceIndex(0.6)).toBe(false)
-    })
-  })
->>>>>>> :white_check_mark: Reorder immutaTest params in core package
   describe('path.pathAlreadyApplied', () => {
     it('should return true if path is included in already applied paths', () => {
       expect(pathAlreadyApplied([
