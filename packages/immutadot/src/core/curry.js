bc30866a328a564ebbef301dfa087100627e26bf
nlepage <19571875+nlepage@users.noreply.github.com>
2018-06-06 19:04:17 +0200

    :ok_hand: @EmrysMyrddin's review

--- packages/immutadot/src/core/curry.js
+++ packages/immutadot/src/core/curry.js
@@ -3,18 +3,21 @@
  * @returns {function|*} Either a new function or the result of the call to the actual function
  * @this Object
  */
-function curried(...args) {
+function curried(...newArgs) {
   const { fn, last, minArity } = this
 
   if (last) {
-    const obj = args.length === 0 ? undefined : args[0]
+    const [obj] = newArgs
     return fn(obj, ...this.args)
   }
 
-  this.args.push(...args)
-  this.last = this.args.length >= minArity - 1
+  const args = this.args.concat(newArgs)
 
-  return curried.bind(this)
+  return curried.bind({
+    ...this,
+    args,
+    last: args.length >= minArity - 1,
+  })
 }
 
 /**
@@ -27,10 +30,11 @@ function maybeCurried(...args) {
 
   if (args.length >= minArity) return fn(...args)
 
-  this.last = args.length === minArity - 1
-  this.args = args
-
-  return curried.bind(this)
+  return curried.bind({
+    ...this,
+    last: args.length === minArity - 1,
+    args,
+  })
 }
 
 export function curry(fn, minArity = fn.length) {
