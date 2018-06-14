ea759622567079dccb0b6383891176fd12f358fd
nlepage <19571875+nlepage@users.noreply.github.com>
2018-06-15 00:12:33 +0200

    :sparkles: Resolve getters in new apply

--- packages/immutadot/src/core/apply.js
+++ packages/immutadot/src/core/apply.js
@@ -1,10 +1,15 @@
 import { curry } from './curry'
+import { isGetter } from './get'
 import { nav } from 'nav/nav'
 import { toPath } from '@immutadot/parser'
 
 function apply(fn, arity = fn.length) {
   return curry(
-    (obj, path, ...args) => nav(toPath(path))(obj).update(value => fn(value, ...args)),
+    (obj, path, ...args) => {
+      const resolvedArgs = args.map(arg => arg[isGetter] ? arg(obj) : arg)
+      const updater = value => fn(value, ...resolvedArgs)
+      return nav(toPath(path))(obj).update(updater)
+    },
     arity + 1, // Add obj and path but remove value
   )
 }
