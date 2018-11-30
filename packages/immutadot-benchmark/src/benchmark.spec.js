42b9ebfbe2d56218e11966c5a1e541eba90a827b
nlepage <19571875+nlepage@users.noreply.github.com>
2018-11-30 21:52:32 +0100

    :zap: Rewrite curry again...

--- packages/immutadot-benchmark/src/benchmark.spec.js
+++ packages/immutadot-benchmark/src/benchmark.spec.js
@@ -19,9 +19,9 @@ const benchmarkSuite = new BenchmarkSuite(
 
 describe('Benchmark suite', () => {
   describe('Set a property', () => setProp(benchmarkSuite))
-  describe('Update small todos list', () => updateTodos(benchmarkSuite, 'Update small todos list (1000 items)', 1000, 100, 30, 50000))
-  describe('Update medium todos list', () => updateTodos(benchmarkSuite, 'Update medium todos list (10000 items)', 10000, 1000, 30, 5000))
-  describe('Update large todos list', () => updateTodos(benchmarkSuite, 'Update large todos list (100000 items)', 100000, 10000, 30, 500))
+  // describe('Update small todos list', () => updateTodos(benchmarkSuite, 'Update small todos list (1000 items)', 1000, 100, 30, 50000))
+  // describe('Update medium todos list', () => updateTodos(benchmarkSuite, 'Update medium todos list (10000 items)', 10000, 1000, 30, 5000))
+  // describe('Update large todos list', () => updateTodos(benchmarkSuite, 'Update large todos list (100000 items)', 100000, 10000, 30, 500))
 
   afterAll(() => benchmarkSuite.log())
 })
--- packages/immutadot/src/core/curry.js
+++ packages/immutadot/src/core/curry.js
@@ -1,30 +1,42 @@
-export function curry(fn, arity = fn.length, fixedArity = false) {
-  return (...args) => {
-    if (args.length >= arity) return fn(...args)
+const curried21 = fn => arg1 => arg0 => fn(arg0, arg1)
 
-    function curried(prevArgs) {
-      return (...args) => {
-        if (prevArgs.length >= arity - 1) {
-          if (fixedArity) {
-            switch (arity) {
-            case 2:
-              return fn(args[0], prevArgs[0])
-            case 3:
-              return fn(args[0], prevArgs[0], prevArgs[1])
-            case 4:
-              return fn(args[0], prevArgs[0], prevArgs[1], prevArgs[2])
-            case 5:
-              return fn(args[0], prevArgs[0], prevArgs[1], prevArgs[2], prevArgs[3])
-            }
-          }
+const curried32 = fn => (arg1, arg2) => arg0 => fn(arg0, arg1, arg2)
+const curried31 = fn => arg1 => (...args) => {
+  if (args.length === 2) return fn(args[1], arg1, args[0])
+  return curried32(fn)(arg1, args[0])
+}
+const curried3 = fn => (...args) => {
+  if (args.length === 2) return curried32(fn)(...args)
+  return curried31(fn)(...args)
+}
 
-          return fn(args[0], ...prevArgs)
-        }
+const curriedN = (fn, arity) => (...firstArgs) => {
+  const curried = (...prevArgs) => (...args) => {
+    if (prevArgs.length >= arity - 1)
+      return fn(args[0], ...prevArgs)
 
-        return curried(prevArgs.concat(args))
-      }
-    }
-
-    return curried(args)
+    return curried(...prevArgs, ...args)
   }
+
+  return curried(...firstArgs)
+}
+
+export function curry(fn, arity = fn.length, fixedArity = false) {
+  let curried
+
+  if (fixedArity) {
+    switch (arity) {
+    case 2:
+      curried = curried21(fn)
+      break
+    case 3:
+      curried = curried3(fn)
+      break
+    }
+  }
+
+  if (!curried) curried = curriedN(fn, arity)
+
+  // TODO Should we truncate args when fixedArity (when calling fn)
+  return (...args) => args.length >= arity ? fn(...args) : curried(...args)
 }
