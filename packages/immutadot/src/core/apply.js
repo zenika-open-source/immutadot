71b96266600e475f97b43e5c11ed63e278b02fbd
nlepage <19571875+nlepage@users.noreply.github.com>
2018-11-30 13:12:58 +0100

    :ok_hand: Review @frinyvonnick

--- packages/immutadot/src/core/apply.js
+++ packages/immutadot/src/core/apply.js
@@ -3,46 +3,50 @@ import { nav } from 'nav/nav'
 import { resolveGetter } from './get'
 import { toPath } from '@immutadot/parser'
 
+const getResolveArg = lazy => lazy ? resolveGetter : arg => arg
+
 function apply(fn, { arity = fn.length, fixedArity = false, curried = true, lazy = true } = {}) {
-  let iFn
+  const resolveArg = getResolveArg(lazy)
+
+  let appliedFn
 
   if (fixedArity) {
     switch (arity) {
     case 0:
     case 1:
-      iFn = (obj, path) => nav(toPath(path))(obj).update(fn)
+      appliedFn = (obj, path) => nav(toPath(path))(obj).update(fn)
       break
     case 2:
-      iFn = (obj, path, arg) => nav(toPath(path))(obj).update(
-        value => fn(value, lazy ? resolveGetter(arg, obj) : arg),
+      appliedFn = (obj, path, arg) => nav(toPath(path))(obj).update(
+        value => fn(value, lazy ? resolveArg(arg, obj) : arg),
       )
       break
     case 3:
-      iFn = (obj, path, arg1, arg2) => nav(toPath(path))(obj).update(
+      appliedFn = (obj, path, arg1, arg2) => nav(toPath(path))(obj).update(
         value => fn(
           value,
-          lazy ? resolveGetter(arg1, obj) : arg1,
-          lazy ? resolveGetter(arg2, obj) : arg2,
+          lazy ? resolveArg(arg1, obj) : arg1,
+          lazy ? resolveArg(arg2, obj) : arg2,
         ),
       )
       break
     case 4:
-      iFn = (obj, path, arg1, arg2, arg3) => nav(toPath(path))(obj).update(
+      appliedFn = (obj, path, arg1, arg2, arg3) => nav(toPath(path))(obj).update(
         value => fn(
           value,
-          lazy ? resolveGetter(arg1, obj) : arg1,
-          lazy ? resolveGetter(arg2, obj) : arg2,
-          lazy ? resolveGetter(arg3, obj) : arg3,
+          lazy ? resolveArg(arg1, obj) : arg1,
+          lazy ? resolveArg(arg2, obj) : arg2,
+          lazy ? resolveArg(arg3, obj) : arg3,
         ),
       )
       break
     }
   }
 
-  if (!iFn) {
-    iFn = (obj, path, ...args) => {
+  if (!appliedFn) {
+    appliedFn = (obj, path, ...args) => {
       let resolvedArgs = args
-      if (lazy) resolvedArgs = args.map(arg => resolveGetter(arg, obj))
+      if (lazy) resolvedArgs = args.map(arg => resolveArg(arg, obj))
       const updater = value => fn(value, ...resolvedArgs)
       return nav(toPath(path))(obj).update(updater)
     }
@@ -51,9 +55,9 @@ function apply(fn, { arity = fn.length, fixedArity = false, curried = true, lazy
   if (curried)
     // Add obj and path but remove value in arity
     // TODO try improving curry when fixedArity is true
-    return curry(iFn, arity + 1)
+    return curry(appliedFn, arity + 1)
 
-  return iFn
+  return appliedFn
 }
 
 export { apply }
