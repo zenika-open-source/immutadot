40e631130ad2375c5ee5f4eabd77c3541accbab0
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2018-11-30 13:15:05 +0100

    :zap: Improve perf of apply functions fix #310 (#315)
    * :zap: Start investigating performance drop...
    
    * :rewind: Do not use apply in core functions for performance
    
    * :zap: Apply version that actually has good perf
    
    * :ok_hand: Review @frinyvonnick

--- packages/immutadot/src/core/apply.js
+++ packages/immutadot/src/core/apply.js
@@ -1,17 +1,63 @@
 import { curry } from './curry'
-import { isGetter } from './get'
 import { nav } from 'nav/nav'
+import { resolveGetter } from './get'
 import { toPath } from '@immutadot/parser'
 
-function apply(fn, arity = fn.length) {
-  return curry(
-    (obj, path, ...args) => {
-      const resolvedArgs = args.map(arg => arg[isGetter] ? arg(obj) : arg)
+const getResolveArg = lazy => lazy ? resolveGetter : arg => arg
+
+function apply(fn, { arity = fn.length, fixedArity = false, curried = true, lazy = true } = {}) {
+  const resolveArg = getResolveArg(lazy)
+
+  let appliedFn
+
+  if (fixedArity) {
+    switch (arity) {
+    case 0:
+    case 1:
+      appliedFn = (obj, path) => nav(toPath(path))(obj).update(fn)
+      break
+    case 2:
+      appliedFn = (obj, path, arg) => nav(toPath(path))(obj).update(
+        value => fn(value, lazy ? resolveArg(arg, obj) : arg),
+      )
+      break
+    case 3:
+      appliedFn = (obj, path, arg1, arg2) => nav(toPath(path))(obj).update(
+        value => fn(
+          value,
+          lazy ? resolveArg(arg1, obj) : arg1,
+          lazy ? resolveArg(arg2, obj) : arg2,
+        ),
+      )
+      break
+    case 4:
+      appliedFn = (obj, path, arg1, arg2, arg3) => nav(toPath(path))(obj).update(
+        value => fn(
+          value,
+          lazy ? resolveArg(arg1, obj) : arg1,
+          lazy ? resolveArg(arg2, obj) : arg2,
+          lazy ? resolveArg(arg3, obj) : arg3,
+        ),
+      )
+      break
+    }
+  }
+
+  if (!appliedFn) {
+    appliedFn = (obj, path, ...args) => {
+      let resolvedArgs = args
+      if (lazy) resolvedArgs = args.map(arg => resolveArg(arg, obj))
       const updater = value => fn(value, ...resolvedArgs)
       return nav(toPath(path))(obj).update(updater)
-    },
-    arity + 1, // Add obj and path but remove value
-  )
+    }
+  }
+
+  if (curried)
+    // Add obj and path but remove value in arity
+    // TODO try improving curry when fixedArity is true
+    return curry(appliedFn, arity + 1)
+
+  return appliedFn
 }
 
 export { apply }
