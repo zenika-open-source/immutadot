b21063e5fab68422149d331406ccb0d553ee9698
nlepage <19571875+nlepage@users.noreply.github.com>
2018-11-30 16:18:42 +0100

    :bug: fix curry when reusing partial calls

--- packages/immutadot/src/core/curry.js
+++ packages/immutadot/src/core/curry.js
@@ -2,31 +2,29 @@ export function curry(fn, arity = fn.length, fixedArity = false) {
   return (...args) => {
     if (args.length >= arity) return fn(...args)
 
-    const allArgs = args
-
-    function curried(...args) {
-      if (allArgs.length >= arity - 1) {
-        if (fixedArity) {
-          switch (arity) {
-          case 2:
-            return fn(args[0], allArgs[0])
-          case 3:
-            return fn(args[0], allArgs[0], allArgs[1])
-          case 4:
-            return fn(args[0], allArgs[0], allArgs[1], allArgs[2])
-          case 5:
-            return fn(args[0], allArgs[0], allArgs[1], allArgs[2], allArgs[3])
+    function curried(prevArgs) {
+      return (...args) => {
+        if (prevArgs.length >= arity - 1) {
+          if (fixedArity) {
+            switch (arity) {
+            case 2:
+              return fn(args[0], prevArgs[0])
+            case 3:
+              return fn(args[0], prevArgs[0], prevArgs[1])
+            case 4:
+              return fn(args[0], prevArgs[0], prevArgs[1], prevArgs[2])
+            case 5:
+              return fn(args[0], prevArgs[0], prevArgs[1], prevArgs[2], prevArgs[3])
+            }
           }
+
+          return fn(args[0], ...prevArgs)
         }
 
-        return fn(args[0], ...allArgs)
+        return curried(prevArgs.concat(args))
       }
-
-      allArgs.push(...args)
-
-      return curried
     }
 
-    return curried
+    return curried(args)
   }
 }
