206d90b10925b79e9ee651245d1cd1b32c862e07
nlepage <19571875+nlepage@users.noreply.github.com>
2018-12-04 08:41:04 +0100

    :recycle: Change applyStringMethod signature

--- packages/immutadot/src/string/substring.spec.js
+++ packages/immutadot/src/string/substring.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 import { immutaTest } from 'test.utils'
 import { substring } from 'string'
+
 describe('string.substring', () => {
   it('should return a slice to the end', () => {
     immutaTest({ nested: { prop: 'Hello World !' } }, ['nested.prop'], (input, path) => {
@@ -8,10 +9,23 @@ describe('string.substring', () => {
       expect(output).toEqual({ nested: { prop: 'World !' } })
       return output
     })
+
+    immutaTest({ nested: { prop: 'Hello World !' } }, ['nested.prop'], (input, path) => {
+      const output = substring(path)(6)(input)
+      expect(output).toEqual({ nested: { prop: 'World !' } })
+      return output
+    })
   })
+
   it('should return a slice', () => {
     immutaTest({ nested: { prop: 'Hello World !' } }, ['nested.prop'], (input, path) => {
       const output = substring(input, path, 6, 11)
+      expect(output).toEqual({ nested: { prop: 'World' } })
+      return output
+    })
+
+    immutaTest({ nested: { prop: 'Hello World !' } }, ['nested.prop'], (input, path) => {
+      const output = substring(path)(6, 11)(input)
       expect(output).toEqual({ nested: { prop: 'World' } })
       return output
     })
