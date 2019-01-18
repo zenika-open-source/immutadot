64a63ec45ae6c61e9f161892f9d8a7f78ad0010f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-01-18 15:46:12 +0100

    :zap: Add setDeepProp in benchmark (#324)

--- packages/immutadot-benchmark/src/benchmark.spec.js
+++ packages/immutadot-benchmark/src/benchmark.spec.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 import { BenchmarkSuite } from './benchmark'
+import { setDeepProp } from './setDeepProp'
 import { setProp } from './setProp'
 import { updateTodos } from './updateTodos'
 
@@ -9,7 +10,7 @@ const benchmarkSuite = new BenchmarkSuite(
     ['es2015', 'ES2015 destructuring'],
     ['immutable', 'immutable 3.8.2'],
     ['seamless', 'seamless-immutable 7.1.3'],
-    ['immer', 'immer 1.2.0'],
+    ['immer', 'immer 1.10.5'],
     ['qim', 'qim 0.0.52'],
     ['immutadot', 'immutad●t 2.0.0'],
     ['qim-curried', 'qim 0.0.52 curried'],
@@ -19,6 +20,7 @@ const benchmarkSuite = new BenchmarkSuite(
 
 describe('Benchmark suite', () => {
   describe('Set a property', () => setProp(benchmarkSuite))
+  describe('Set a deeply nested property', () => setDeepProp(benchmarkSuite))
   describe('Update small todos list', () => updateTodos(benchmarkSuite, 'Update small todos list (1000 items)', 1000, 100, 30, 50000))
   describe('Update medium todos list', () => updateTodos(benchmarkSuite, 'Update medium todos list (10000 items)', 10000, 1000, 30, 5000))
   describe('Update large todos list', () => updateTodos(benchmarkSuite, 'Update large todos list (100000 items)', 100000, 10000, 30, 500))
