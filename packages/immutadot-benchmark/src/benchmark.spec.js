b6b470e8c1c688796cad378a86a567ebedf5bea6
nlepage <19571875+nlepage@users.noreply.github.com>
2019-01-25 15:13:28 +0100

    :arrow_up: Minor upgrades

--- packages/immutadot-benchmark/src/benchmark.spec.js
+++ packages/immutadot-benchmark/src/benchmark.spec.js
@@ -10,7 +10,7 @@ const benchmarkSuite = new BenchmarkSuite(
     ['es2015', 'ES2015 destructuring'],
     ['immutable', 'immutable 3.8.2'],
     ['seamless', 'seamless-immutable 7.1.4'],
-    ['immer', 'immer 1.12.0'],
+    ['immer', 'immer 1.12.1'],
     ['qim', 'qim 0.0.52'],
     ['immutadot', 'immutad●t 2.0.0'],
     ['qim-curried', 'qim 0.0.52 curried'],
