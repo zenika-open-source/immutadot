3efe2395831963b6156c9ed06731e521aa62b5d1
nlepage <19571875+nlepage@users.noreply.github.com>
2019-01-25 11:37:52 +0100

    :wrench: Move tsconfig at parser's root

--- packages/immutadot-parser/config/jest.js
+++ packages/immutadot-parser/config/jest.js
@@ -1,7 +1,7 @@
 const rootDir = process.cwd()
 
 module.exports = {
-  globals: { 'ts-jest': { tsConfig: 'config/tsconfig.json' } },
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
   preset: 'ts-jest',
   rootDir,
   testEnvironment: 'node',
