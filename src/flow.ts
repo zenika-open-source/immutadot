eceff20f5cdcbb9059d2d6a1fd4bcd0410cdbc89
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-11-18 23:01:25 +0100

    :recycle: No map in flow

--- src/flow.ts
+++ src/flow.ts
@@ -3,6 +3,6 @@ import { useRefs } from './make'
 export function flow(...fns: ((value: any) => any)[]): (value: any) => any {
   return (value) => {
     const refs = new Set()
-    return fns.map((fn) => fn[useRefs]?.(refs) ?? fn).reduce((res, fn) => fn(res), value)
+    return fns.reduce((res, fn) => (fn[useRefs]?.(refs) ?? fn)(res), value)
   }
 }
