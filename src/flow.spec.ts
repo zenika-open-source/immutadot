e22634917089cd95df32cf84270c5accb459dde7
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-12-02 22:34:41 +0100

    ✅

--- src/flow.spec.ts
+++ src/flow.spec.ts
@@ -1,6 +1,7 @@
 import { recurFreeze } from '../recurFreeze'
 import { flow } from './flow'
 import { set } from './set'
+import { update } from './update'
 
 describe('flow', () => {
   it('should set several paths', () => {
@@ -15,6 +16,25 @@ describe('flow', () => {
       set`.nested.property`('aze'),
       set`.nested.otherProperty`('qwe'),
     )(o)).toEqual({
+      nested: {
+        property: 'aze',
+        otherProperty: 'qwe',
+      },
+    })
+  })
+
+  it('should be composable', () => {
+    const o = recurFreeze({
+      nested: {
+        property: 'foo',
+        otherProperty: 'baz',
+      },
+    })
+
+    expect(update`.nested`(flow(
+      set`.property`('aze'),
+      set`.otherProperty`('qwe'),
+    ))(o)).toEqual({
       nested: {
         property: 'aze',
         otherProperty: 'qwe',
