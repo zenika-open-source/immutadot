6f79abe613200d7a5f1d056d030ea0d3ab38fa1a
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-03-17 11:43:09 +0100

    Build using vite 🗲

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'immutadot',
+    },
+  },
+  plugins: [dts()],
+})
